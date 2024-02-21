a05f2d82feab4d5bf626148774a297bfb1fa0e86
Goose <victoriaplatt@loveplanes.co.uk>
2024-02-21 16:27:30 +0000

    Complete EditUser page

--- src/Pages/EditUser.tsx
+++ src/Pages/EditUser.tsx
@@ -65,95 +65,95 @@ export default function EditUser({ users, setUsers, handleDelete }: EditUserProp
     };
 
     return (
-        <section className="min-h-screen py-12">
-            <div className="max-w-3xl mx-auto bg-gray-500 rounded-lg shadow-lg p-8">
-                <h1 className="text-3xl font-bold mb-4 text-white">View User</h1>
+        <section className="bg-orange-400 min-h-screen py-12">
+            <div className="max-w-3xl mx-auto bg-blue-200 rounded-lg shadow-lg p-8">
+                <h1 className="text-3xl font-bold mb-4 text-blue-900">😸Update Cat😸</h1>
                 {user ? (
                     <form onSubmit={handleSubmit}>
                         <div className="grid grid-cols-2 gap-4">
                             <div className="col-span-2 sm:col-span-1">
-                                <label className="block text-sm font-medium text-white">First Name</label>
+                                <label className="block text-sm font-medium text-gray-700">First Name</label>
                                 <input
                                     type="text"
                                     name="firstName"
                                     value={formValues?.firstName || ''}
                                     onChange={handleInputChange}
-                                    className="mt-1 bg-white text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                    className="mt-1 bg-blue-300 text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                 />
                             </div>
                             <div className="col-span-2 sm:col-span-1">
-                                <label className="block text-sm font-medium text-white">Last Name</label>
+                                <label className="block text-sm font-medium text-gray-700">Last Name</label>
                                 <input
                                     type="text"
                                     name="lastName"
                                     value={formValues?.lastName || ''}
                                     onChange={handleInputChange}
-                                    className="mt-1 bg-white text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                    className="mt-1 bg-blue-300 text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                 />
                             </div>
                             <div className="col-span-2 sm:col-span-1">
-                                <label className="block text-sm font-medium text-white">Email</label>
+                                <label className="block text-sm font-medium text-gray-700">Email</label>
                                 <input
                                     type="email"
                                     name="email"
                                     value={formValues?.email || ''}
                                     onChange={handleInputChange}
-                                    className="mt-1 bg-white text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                    className="mt-1 bg-blue-300 text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                 />
                             </div>
                             <div className="col-span-2 sm:col-span-1">
-                                <label className="block text-sm font-medium text-white">Phone</label>
+                                <label className="block text-sm font-medium text-gray-700">Phone</label>
                                 <input
                                     type="text"
                                     name="phone"
                                     value={formValues?.phone || ''}
                                     onChange={handleInputChange}
-                                    className="mt-1 bg-white text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                    className="mt-1 bg-blue-300 text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                 />
                             </div>
                             <div className="col-span-2 sm:col-span-1">
-                                <label className="block text-sm font-medium text-white">Address</label>
+                                <label className="block text-sm font-medium text-gray-700">Address</label>
                                 <input
                                     type="text"
                                     name="address"
                                     value={formValues?.address?.address || ''}
                                     onChange={handleInputChange}
-                                    className="mt-1 bg-white text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                    className="mt-1 bg-blue-300 text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                 />
                                 <input
                                     type="text"
                                     name="city"
                                     value={formValues?.address?.city || ''}
                                     onChange={handleInputChange}
-                                    className="mt-1 bg-white text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                    className="mt-1 bg-blue-300 text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                 />
                                 <input
                                     type="text"
                                     name="postalCode"
                                     value={formValues?.address?.postalCode || ''}
                                     onChange={handleInputChange}
-                                    className="mt-1 bg-white text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                    className="mt-1 bg-blue-300 text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                 />
                                 <input
                                     type="text"
                                     name="state"
                                     value={formValues?.address?.state || ''}
                                     onChange={handleInputChange}
-                                    className="mt-1 bg-white text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                    className="mt-1 bg-blue-300 text-gray focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                                 />
                             </div>
                         </div>
                         <div className="mt-4 flex justify-start space-x-8">
-                            <button type="submit" className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-400 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                Update User
+                            <button type="submit" className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-400 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                Update Cat
                             </button>
-                            <button type="button" onClick={handleDeleteClick} className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-400 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                                Delete User
+                            <button type="button" onClick={handleDeleteClick} className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-400 hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                                Delete Cat 😿
                             </button>
                         </div>
                     </form>
                 ) : (
-                    <p>Loading...</p>
+                    <p>Loading Business Cat 😺...</p>
                 )}
                 <button className="block mt-4 bg-gray-600 text-white px-4 py-2 rounded-md" onClick={() => navigate('/')}>Back</button>
             </div>
